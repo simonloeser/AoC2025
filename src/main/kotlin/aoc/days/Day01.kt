c6a32802d84e4ff146de05266c3dd3e637372614
simonloeser <92864888+simonloeser@users.noreply.github.com>
2025-12-01 15:28:49 +0100

    feat(day01): add part 1

--- src/main/kotlin/aoc/days/Day01.kt
+++ src/main/kotlin/aoc/days/Day01.kt
@@ -4,7 +4,19 @@ import aoc.Day
 
 class Day01 : Day(1) {
     override fun part1(): Any {
-        return "day 1 part 1 test"
+        var dial = 50
+        var dialPointedAtZero = 0
+
+        for (instruction in input) {
+            val n = instruction.drop(1).toInt()
+            dial = if (instruction.startsWith("L")) {
+                (dial - n).mod(100)
+            } else {
+                (dial + n).mod(100)
+            }
+            dialPointedAtZero += if (dial == 0) 1 else 0
+        }
+        return "day 1 part 1: $dialPointedAtZero"
     }
 
     override fun part2(): Any {
