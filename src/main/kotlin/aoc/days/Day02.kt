bcbb61d703c039a618fb6f25e5e038832d16c083
simonloeser <92864888+simonloeser@users.noreply.github.com>
2025-12-02 19:52:15 +0100

    feat(day02): add part 1

--- src/main/kotlin/aoc/days/Day02.kt
+++ src/main/kotlin/aoc/days/Day02.kt
@@ -4,10 +4,23 @@ import aoc.Day
 
 class Day02 : Day(2) {
     override fun part1(): Any {
-        return 0
+        val invalidIds = mutableListOf<Long>()
+        val ranges = input[0].split(",").filter { it.isNotBlank() }
+
+        for (r in ranges) {
+            val (startStr, endStr) = r.split("-")
+
+            for (id in startStr.toLong()..endStr.toLong()) {
+                if (isRepeatedTwice(id.toString())) invalidIds.add(id)
+            }
+        }
+        return "Sum of invalid IDs: ${invalidIds.sum()}"
     }
 
     override fun part2(): Any {
         return 0
     }
+
+    fun isRepeatedTwice(id: String): Boolean =
+        id.length % 2 == 0 && id.take(id.length / 2) == id.drop(id.length / 2)
 }
