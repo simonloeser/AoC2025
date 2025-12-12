50f55b794cac6e8a4406be5e465834c30b82b420
simonloeser <92864888+simonloeser@users.noreply.github.com>
2025-12-12 13:46:01 +0100

    feat(day03): add part 2

--- src/main/kotlin/aoc/days/Day03.kt
+++ src/main/kotlin/aoc/days/Day03.kt
@@ -1,13 +1,14 @@
 package aoc.days
 
 import aoc.Day
+import java.math.BigInteger
 
 class Day03 : Day(3) {
     override fun part1(): Any {
-        var sumOfJoltages: Int = 0
+        var sumOfJoltages = 0
         for (line in input) {
             val digits = line.map { it.digitToInt() }
-            var bestJoltage: Int = 0
+            var bestJoltage = 0
             for (i in digits.indices) {
                 for (j in i + 1 until digits.size) {
                     val joltage = digits[i] * 10 + digits[j]
@@ -20,6 +21,25 @@ class Day03 : Day(3) {
     }
 
     override fun part2(): Any {
-        return 0
+        var sumOfJoltages = BigInteger.ZERO
+
+        for (line in input) {
+            val n = 12
+            val digits = line.map { it.digitToInt() }
+            val stack = mutableListOf<Int>()
+
+            for ((i, d) in digits.withIndex()) {
+                while (stack.isNotEmpty() && stack.last() < d && stack.size + digits.size - i - 1 >= n) {
+                    stack.removeAt(stack.size - 1)
+                }
+                if (stack.size < n) {
+                    stack.add(d)
+                }
+            }
+
+            val largest = BigInteger(stack.joinToString(""))
+            sumOfJoltages += largest
+        }
+        return sumOfJoltages
     }
 }
